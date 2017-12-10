eee7b93cfdc681d30be7909170065ac30e68039f
Lamine <lemzoba@gmail.com>
2017-12-10 23:26:45 +0100

    feat: remove unused `key` on the dict of the EventHandlerManager

--- app/events/events_handler_template.py
+++ app/events/events_handler_template.py
@@ -1,54 +1,26 @@
-from flask import current_app
-
-
-def canceled_message_and_send_mail(message, exception, origin=None):
-    if 'mail_service' not in current_app.extensions:
-        current_app.logger.warning("Extension 'mail_service' not initialized")
-        return 'CANCELLED'  # TODO : choose a different status ?
-
-    subject = 'Message %s CANCELLED par le système' % str(message.id)
-    body = """Bonjour,
-Le message (id : {id}) de la file {queue} a été automatiquement passé en CANCELLED par le système.
-
-Exception: {error}
-
-Cordialement,
--- Mailer Daemon""".format(id=str(message.id), queue=str(origin), error=str(exception))
-
-    recipients = {}  # [utilisateur.email for utilisateur in administrateurs_nationaux_valides]
-
-    mail = current_app.extensions['mail_service']
-    mail.send_threaded(subject=subject, recipients=recipients, body=body)
-    return 'CANCELLED'
-
-
 EVENT_HANDLERS_TEMPLATE = [
     {
         'label': 'Register a user',
         'queue': 'user',
         'processor': 'register_processor',
-        'event': 'user.create',
-        'on_error_callback': canceled_message_and_send_mail
+        'event': 'user.create'
     },
     {
         'label': 'View user',
         'queue': 'user',
         'processor': 'count_user_viewing_processor',
-        'event': 'user.read',
-        'on_error_callback': canceled_message_and_send_mail
+        'event': 'user.read'
     },
     {
         'label': 'Update user',
         'queue': 'user',
         'processor': 'notify_user_update',
-        'event': 'user.update',
-        'on_error_callback': canceled_message_and_send_mail
+        'event': 'user.update'
     },
     {
         'label': 'Delete a user',
         'queue': 'user',
         'processor': 'disable_user_processor',
-        'event': 'user.delete',
-        'on_error_callback': canceled_message_and_send_mail
+        'event': 'user.delete'
     },
 ]
--- broker_rabbit/event_handler.py
+++ broker_rabbit/event_handler.py
@@ -1,17 +1,4 @@
-from datetime import datetime, timedelta
-from traceback import format_exc
-
 from broker_rabbit.processor import find_and_execute
-
-
-STATUS_FAILURE = 'FAILURE'
-STATUS_CANCELLED = 'CANCELLED'
-STATUS_SKIPPED = 'SKIPPED'
-STATUS_DONE = 'DONE'
-STATUS_DELETED = 'DELETED'
-
-BASE_DELAY = 60
-MAX_DELAY = 6 * 60 * 60  # 6 hours
 
 
 class EventError(Exception):
@@ -26,10 +13,6 @@ class UnknownEventError(EventError):
     pass
 
 
-def default_callback(message, exception, origin):
-    return STATUS_FAILURE
-
-
 class EventHandlerItem:
 
     def __init__(self, event_handler_item_data):
@@ -37,34 +20,16 @@ class EventHandlerItem:
         self.queue = event_handler_item_data.get('queue')
         self.processor = event_handler_item_data.get('processor')
         self.event = event_handler_item_data.get('event')
-        self.to_skip = event_handler_item_data.get('to_skip')
-        self.on_error_callback = event_handler_item_data.get('on_error_callback', default_callback)
 
     def modify(self, *args, **kwargs):
         if 'label' in kwargs:
             self.label = kwargs['label']
-        if 'origin' in kwargs:
-            self.origin = kwargs['origin']
         if 'queue' in kwargs:
             self.queue = kwargs['queue']
         if 'processor' in kwargs:
             self.processor = kwargs['processor']
-        if 'processor_batch_in' in kwargs:
-            self.processor_batch_in = kwargs['processor_batch_in']
-        if 'processor_batch_out' in kwargs:
-            self.processor_batch_out = kwargs['processor_batch_out']
-        if 'context' in kwargs:
-            self.context = kwargs['context']
         if 'event' in kwargs:
             self.event = kwargs['event']
-        if 'to_skip' in kwargs:
-            self.event = kwargs['to_skip']
-        if 'on_error_callback' in kwargs:
-            self.on_error_callback = kwargs['on_error_callback']
-        if 'to_rabbit' in kwargs:
-            self.to_rabbit = kwargs['to_rabbit']
-        if 'to_batch' in kwargs:
-            self.to_batch = kwargs['to_batch']
 
 
 class EventHandler:
@@ -75,8 +40,8 @@ class EventHandler:
 
     def process_message(self, received_msg):
         self.count_received_msg += 1
-        # result_msg = str(find_and_execute(handler.processor, handler, msg))
         print('Received message number `%s`, content = {%s}' % (self.count_received_msg, received_msg))
+        # find_and_execute(handler.processor, handler, msg)
 
     def get(self, label, default=None):
         try:
@@ -90,12 +55,10 @@ class EventHandler:
                 return item
         raise IndexError()
 
-    def filter(self, event=None, origin=None):
+    def filter(self, event=None):
         ret = self.items
         if event:
             ret = [x for x in ret if x.event == event]
-        if origin:
-            ret = [x for x in ret if x.origin != origin]
         return ret
 
     def flush(self):
