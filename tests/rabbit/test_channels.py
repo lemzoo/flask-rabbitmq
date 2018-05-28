f56fed1012a040ce7c329df52c484ace9c3babfb
lemzoo <lemzobba@gmail.com>
2018-05-28 09:59:36 +0200

    test: update test to be working

--- tests/rabbit/test_channels.py
+++ tests/rabbit/test_channels.py
@@ -5,36 +5,85 @@ import pytest
 from pika.connection import Connection
 from pika.spec import Basic
 
-from tests.base_test import RabbitBrokerTest
-from broker.broker_rabbit.exceptions import (ConnectionNotOpenedYet,
-                                             WorkerExitException,
-                                             ConnectionIsClosed)
-from broker.broker_rabbit.rabbit.channels import ChannelHandler, WorkerChannel, ProducerChannel
-from broker.broker_rabbit.rabbit.connection_handler import ConnectionHandler
+from broker_rabbit.exceptions import (
+    ConnectionNotOpenedYet, ConnectionIsClosed, WorkerExitException,
+    ChannelNotDefinedError)
+
+from broker_rabbit.channels import (
+    ChannelHandler, WorkerChannel, ProducerChannel)
+from broker_rabbit.connection_handler import ConnectionHandler
+
 from tests import common
+from tests.base_test import RabbitBrokerTest
 
 
 @pytest.mark.functional_test
 class TestChannelHandler(RabbitBrokerTest):
     def setup_method(self):
-        self.connection = ConnectionHandler(common.get_rabbit_url())
-        self.channel_handler = ChannelHandler(self.connection.get_current_connection())
+        self.connection = ConnectionHandler(common.BROKER_URL_TEST)
+        current_connection = self.connection.get_current_connection()
+        self.channel_handler = ChannelHandler(current_connection)
 
-    def test_if_channel_is_open(self):
-        self.channel_handler.open()
-        assert self.connection._connection.is_open
-        self.channel_handler.close()
+    def test_should_raise_when_connection_is_not_defined(self):
+        # Given
+        connection = None
+        channel_handler = ChannelHandler(connection)
 
-    def test_if_channel_is_open_failed(self):
-        with pytest.raises(ConnectionNotOpenedYet):
-            channel_handler = ChannelHandler(None)
+        # When
+        with pytest.raises(ConnectionNotOpenedYet) as error:
             channel_handler.open()
 
-    def test_if_connection_is_closed(self):
-        with pytest.raises(ConnectionIsClosed):
-            self.channel_handler._connection.close()
-            assert self.connection._connection.is_closed
+        # Then
+        assert 'The connection is not opened' == error.value.args[0]
+
+    def test_should_raise_when_connection_is_closed(self):
+        # Given
+        self.connection.close_connection()
+
+        # When
+        with pytest.raises(ConnectionIsClosed) as error:
             self.channel_handler.open()
+
+        # Then
+        assert 'The connection is closed' == error.value.args[0]
+
+    def test_should_open_channel(self):
+        # When
+        self.channel_handler.open()
+
+        # Then
+        assert self.channel_handler.get_channel().is_open is True
+
+    def test_should_close_channel(self):
+        # Given
+        self.channel_handler.open()
+
+        # When
+        self.channel_handler.close()
+
+        # Then
+        assert self.channel_handler.get_channel().is_closed is True
+
+    def test_should_close_channel(self):
+        # Given
+        self.channel_handler.open()
+
+        # When
+        self.channel_handler.close()
+
+        # Then
+        assert self.channel_handler.get_channel().is_closed is True
+
+    def test_should_raise_when_channel_is_not_defined(self):
+        # Given
+        self.channel_handler._channel = None
+
+        # When
+        with pytest.raises(ChannelNotDefinedError) as error:
+            self.channel_handler.get_channel()
+
+        # Then
+        assert 'The channel does not exist yet' == error.value.args[0]
 
 
 @pytest.mark.unit_test
@@ -42,7 +91,7 @@ class TestWorkerChannel:
     def setup_method(self):
         connection = Mock(Connection)
         connection.is_closed = False
-        self.worker_channel = WorkerChannel(connection, None, None, None)
+        self.worker_channel = WorkerChannel(connection, None, None)
         self.worker_channel.open()
 
     def teardown_method(self):
@@ -68,7 +117,9 @@ class TestProducerChannel:
     def setup_method(self):
         connection = Mock(Connection)
         connection.is_closed = False
-        self.producer_channel = ProducerChannel(connection)
+        application_id = 'TEST_APP_ID'
+        delivery_mode = 2
+        self.producer_channel = ProducerChannel(connection, application_id, delivery_mode)
         self.producer_channel.open()
 
     def teardown_method(self):
@@ -86,4 +137,5 @@ class TestProducerChannel:
         self.producer_channel.send_message(exchange, queue, message)
 
         self.producer_channel._channel.basic_publish.assert_called_with(
-            exchange=exchange, routing_key=queue, body=body, properties=properties)
+            exchange=exchange, routing_key=queue,
+            body=body, properties=properties)
