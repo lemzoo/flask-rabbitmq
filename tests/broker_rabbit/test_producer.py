a253cda6d250a2737f60279d9f07599ff5841ec7
lemzoo <lemzobba@gmail.com>
2018-06-01 23:24:54 +0200

    test: update producer test after updating the producer

--- tests/broker_rabbit/test_producer.py
+++ tests/broker_rabbit/test_producer.py
@@ -3,29 +3,39 @@ from unittest.mock import Mock
 import pytest
 
 from broker_rabbit.channels import ProducerChannel
+from broker_rabbit.connection_handler import ConnectionHandler
 from broker_rabbit.producer import Producer
+from tests import common
 
 
 @pytest.mark.unit_test
 class TestProducer:
-    def test_publish(self):
+    def setup(self):
+        connection = ConnectionHandler(common.BROKER_URL_TEST)
+        current_connection = connection.get_current_connection()
+        self.exchange_name = 'TEST-EXCHANGE-NAME'
+        self.application_id = 'TEST-APPLICATION-ID'
+        self.delivery_mode = 2
+        channel_handler = ProducerChannel(
+            current_connection, self.application_id, self.delivery_mode)
+        channel_handler.open()
+        self.channel = channel_handler.get_channel()
+        self.channel = Mock(ProducerChannel)
+        self.queues = ['first-queue', 'second-queue']
+        self.producer = Producer(self.channel, self.exchange_name, self.queues)
+
+    def test_should_bootstrap_environment_to_work(self):
+        pass
+
+    def test_should_publish_on_given_queue(self):
         # Given
-        connection = None
-        exchange_name = "TEST_EXCHANGE_NAME"
-        application_id = 'TEST-APPLICATION-ID'
-        delivery_mode = 2
-        producer = Producer(connection, exchange_name, application_id, delivery_mode)
-        queues = ['first-queue', 'second-queue']
-        producer._producer_channel = Mock(ProducerChannel)
-        producer.bootstrap(queues)
-        first_queue = queues[0]
+        first_queue = self.queues[0]
         message = "TEST_MESSAGE"
 
         # When
-        producer.publish(first_queue, message)
+        self.producer.publish(first_queue, message)
 
         # Then
-        channel = producer._producer_channel
-        assert channel.open.called is True
-        channel.send_message.assert_called_with(exchange_name, first_queue, message)
-        assert channel.close.called is True
+        assert self.channel.open.called is True
+        self.channel.send_message.assert_called_with(self.exchange_name, first_queue, message)
+        assert self.channel.close.called is True
