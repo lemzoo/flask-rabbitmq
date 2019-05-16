35b8b3bd5812be35b535b6d1d3fa9ee5b5fbeb96
Lamine <lemzoba@gmail.com>
2019-05-16 20:09:17 +0200

    fix: keep old definition of exception to allow keyword

--- broker_rabbit/exceptions.py
+++ broker_rabbit/exceptions.py
@@ -1,5 +1,6 @@
 class BrokerRabbitException(Exception):
-    pass
+    def __init__(self, args, **kwargs):
+        super().__init__(args, kwargs)
 
 
 class UnknownQueueError(BrokerRabbitException):
