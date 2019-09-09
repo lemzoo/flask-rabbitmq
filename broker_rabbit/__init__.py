caeb7dd2d7fd709d78530d0cdaf04b27e47046fe
Lamine <lemzoba@gmail.com>
2019-09-09 18:58:08 +0200

    refactor: expose only necessary resources

--- broker_rabbit/__init__.py
+++ broker_rabbit/__init__.py
@@ -1,3 +1,10 @@
 from broker_rabbit.broker import BrokerRabbitMQ
+from broker_rabbit.connection_handler import ConnectionHandler
+from broker_rabbit.exceptions import UnknownQueueError
 
-__all__ = ('BrokerRabbitMQ',)
+from broker_rabbit.channels import ProducerChannel
+from broker_rabbit.producer import Producer
+
+
+__all__ = ('BrokerRabbitMQ', 'ConnectionHandler', 'Producer',
+           'ProducerChannel', 'UnknownQueueError')
